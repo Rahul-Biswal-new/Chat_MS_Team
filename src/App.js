bbcada1683e3c88436b0096bcd6c817e45976f43
Rahul-Biswal-new <rahulbiswalnew@gmail.com>
2023-11-28 13:35:23 +0530

    css

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import './App.css';
+import { ChatEngine } from 'react-chat-engine'
+
+
+function App() {
+  return (
+    <div>
+      <ChatEngine
+          height="100vh"
+          projectID="99daef57-20b1-4d20-87f0-888f4166dff3"
+          userName="rahulbiswal"
+          userSecret="1234"
+        />
+    </div>
+  );
+}
+
+export default App;
